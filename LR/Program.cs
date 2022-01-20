bd18c30bf66de6c8503f043e3132bedc9c57be1a
MotyaMiracle <MotyaMiracle@COMPUTER>
2022-01-20 18:39:23 +0300

    Добавьте файлы проекта.

---
+++ LR/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using ClassLibraryS;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LR
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MallardDuck MD = new MallardDuck();
+            RedheadDuck RD = new RedheadDuck();
+            Duck[] duck = new Duck[] { MD, RD };
+
+            for (int i = 0; i < duck.Length; i++)
+            {
+                Console.WriteLine(duck[i].quack());
+                Console.WriteLine(duck[i].swim());
+                Console.WriteLine(duck[i].display());
+            }
+            Console.ReadKey();
+        }
+    }
+}
